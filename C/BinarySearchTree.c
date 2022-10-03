6b4fd7ec08ec7d9b6875bf7ddd8a3b65aacdac48
Zay Davis <112731206+ZayBoyy@users.noreply.github.com>
2022-10-02 22:33:09 -0600

    Not Finished, and certainly broken, but at least attempted

--- C/BinarySearchTree.c
+++ C/BinarySearchTree.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
 
-int main() {
+//Represent a node of a binary tree
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+}typedef Node;
+
+//This starts the root of the binary tree
+struct BinarySearchTree {
+    Node root;
+}typedef BinarySearchTree;
+
+//This should create a new node with the data inputted
+Node construct(int input) {
+Node attempt;
+attempt.data = input;
+return attempt;
+}
+
+//This should calculate the factorial of the input number
+int factorial(int num) {
+    int fact = 1;
+    if (num == 0) {
+        return 1;
+    }
+    else {
+        while(num > 1) {
+            fact = fact * num;
+            num--; 
+        }
+        return fact;
+    }
+}
+
+//This caclulates the number of possible BST's by determining the catalan number for key
+int numOfBST(int key) {    
+    int catalanNumber = factorial(2 * key)/(factorial(key + 1) * factorial(key));    
+    return catalanNumber;    
+}    
+
+
+
+//This is the main 
+int main () {
+    //Display total number of possible binary search trees with key 5
+    printf("Total number of possible Binary Search Trees with given key: %i \n", numOfBST(5));
     return 0;
 }
--- C/SinglyLinkedList.c
+++ C/SinglyLinkedList.c
@@ -1,5 +1,79 @@
 #include <stdio.h>
 
+
+//Made to represent node of a singly linked list
+struct Node{
+    int data;
+    struct Node* next;
+}typedef Node;
+
+
+//Represents begining and end of the linked list
+struct SinglyLinkedList {
+    Node* head;
+    
+    Node* tail;
+    
+}typedef SinglyLinkedList;
+
+
+SinglyLinkedList addNode(SinglyLinkedList list, int data) {
+   
+    Node newNode;
+
+    newNode.data = data;
+
+    if (list.head == NULL) {
+        list.head = &newNode;
+        list.tail = &newNode;
+        printf("If %d",newNode.data);
+        
+    }
+
+    else {printf("Else %d",newNode.data);
+        list.tail->next = &newNode;
+        list.tail = &newNode;
+    }
+}
+
+
+void display(SinglyLinkedList list) {
+
+    Node current = *list.head;
+
+    if (list.head == NULL) {
+        printf("List is empty");
+        return;
+    }
+
+    printf("Nodes of singly linked list: ");
+    while(&current !=  NULL) {
+        //Prints each node by incrementing pointer
+        printf("%d ", current.data);
+        current = *current.next;
+    }
+    printf("\n");
+
+
+}
+
+
 int main() {
+
+    SinglyLinkedList sList;
+
+    printf("%p \n", &sList.head);
+ 
+    addNode(sList, 1);
+    printf("&d \n", sList.head->data);
+   
+    addNode(sList, 2);
+    addNode(sList, 3);
+    addNode(sList, 4);
+
+    
+
+    display(sList);
+
     return 0;
 }
